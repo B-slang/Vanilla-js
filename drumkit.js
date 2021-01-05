13a196c2497581c35d006c6114372f5f8abaae57
B-slang <entr.brijesh@gmail.com>
2021-01-05 20:12:18 +0530

    all major things done

--- drumkit.js
+++ drumkit.js
@@ -1,10 +1,13 @@
 //testing keyboardevent  for now
 window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     console.log(audio); // should log the keyevent object
     if (!audio) return; //stop the fn
+    audio.currentTime = 0;
 
     audio.play();
+    key.classList.add('playing');
 });
 
 
